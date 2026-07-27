7d4f5d820ef4aef8c19c536b67f147e7c07645ac
Ali Hasan <alihasan.dev@gmail.com>
2026-07-27 22:02:51 +0530

    Initiate web implementation

--- lib/features/view_all/presentation/page/view_all_screen.dart
+++ lib/features/view_all/presentation/page/view_all_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -76,7 +77,7 @@ class ViewAllScreen extends HookConsumerWidget {
             return GridView.builder(
               controller: scrollController,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+                crossAxisCount: kIsWeb ? 6 : 3,
                 mainAxisSpacing: 8,
                 crossAxisSpacing: 8,
                 childAspectRatio: 0.7
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'core/routes/app_routes.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  runApp(ProviderScope(child: const MyApp()));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://6069ae5b83b57ab70e5f8ae2c35cd000@o4511802462961664.ingest.us.sentry.io/4511802473381888';
+      // Adds request headers and IP for users, for more info visit:
+      // https://docs.sentry.io/platforms/dart/guides/flutter/data-management/data-collected/
+      options.sendDefaultPii = true;
+      options.enableLogs = true;
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+      // Configure Session Replay
+      options.replay.sessionSampleRate = 0.1;
+      options.replay.onErrorSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: ProviderScope(child: const MyApp()))),
+  );
 }
 
 class MyApp extends StatelessWidget {
